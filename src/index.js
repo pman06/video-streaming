f465a8b9ae3621026f1131f6496c6979a8fb2959
Paul <freemeonline@gmail.com>
2024-04-11 18:21:39 -0500

    Added dev and prod scripts and dependencies

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 const express = require("express");
 const fs = require("fs");
 const app = express();
-const port = 3000;
+
+if (!process.env.PORT) {
+  throw new Error(
+    "Please specify the port number for the HTTP server with the environment variable PORT."
+  );
+}
+const PORT = process.env.PORT;
+
 app.get("/video", async (req, res) => {
   const videoPath = "./videos/SampleVideo_1280x720_1mb.mp4";
   const stats = await fs.promises.stat(videoPath);
@@ -12,4 +19,4 @@ app.get("/video", async (req, res) => {
   fs.createReadStream(videoPath).pipe(res);
 });
 
-app.listen(port, () => console.log(`Server listening on port ${port}`));
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
